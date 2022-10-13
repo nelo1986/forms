366d7a3b43920cf360fc4ff99a0ddbf95fec625e
nelo1986 <ruben.pabo.amores@gmail.com>
2022-10-13 17:15:17 +0200

    fd

--- form_cisco_pue/get_email.php
+++ form_cisco_pue/get_email.php
@@ -10,7 +10,6 @@ $profile = $USER->profile;
 $institution_name = $profile["nombre_centro"];
 $certiportid= $profile["certiportid"];
 
-
 echo "document.getElementById('useremail').value = '".$useremail."';";
 echo "document.getElementById('institution_name').value = '".$institution_name."';";
 echo "document.getElementById('name').value = '".$firstname." ".$lastname."';";
