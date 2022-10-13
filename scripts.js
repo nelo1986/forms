02a0d60b03415865f911053d8bd1b5452921384f
nelo1986 <ruben.pabo.amores@gmail.com>
2022-10-13 13:35:06 +0200

    d

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.getElementById('useremail').value = 'ruben.pabo@pue.es';
+document.getElementById('institution_name').value = 'PUE';
+document.getElementById('name').value = 'Ruben Pabo';
+document.getElementById('certiportid').value = '123';
